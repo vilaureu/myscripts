830cedf7cba5bef601e37e4c021637f97ff13671
vilaureu <vilaureu@users.noreply.github.com>
2025-03-21 14:53:00 +0100

    switch nix formatter to new official one

--- flake.nix
+++ flake.nix
@@ -1,28 +1,37 @@
 {
   description = "Some of my personal scripts that I use quite frequently";
 
-  outputs = { self, nixpkgs, ... }:
+  outputs =
+    { self, nixpkgs, ... }:
     let
       supportedSystems = [ "x86_64-linux" ];
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
       forAllPkgs = f: forAllSystems (system: f nixpkgs.legacyPackages.${system});
     in
     {
-      packages = forAllPkgs (pkgs:
+      packages = forAllPkgs (
+        pkgs:
         let
           # Create a wrapper script with the right PATH and maybe a shell completion.
-          wrap = { name, ... } @ args:
-            (pkgs.writeShellApplication ({ text = ''exec ${./bin/${name}} "$@"''; } // args)).overrideAttrs (oldAttrs: {
-              nativeBuildInputs = (oldAttrs.nativeBuildInputs or [ ]) ++ [ pkgs.installShellFiles ];
-              buildCommand = oldAttrs.buildCommand + ''
-                completion=${./completions}/${name}.fish
-                if [[ -e "$completion" ]] then installShellCompletion "$completion"; fi
-              '';
-            });
+          wrap =
+            { name, ... }@args:
+            (pkgs.writeShellApplication ({ text = ''exec ${./bin/${name}} "$@"''; } // args)).overrideAttrs
+              (oldAttrs: {
+                nativeBuildInputs = (oldAttrs.nativeBuildInputs or [ ]) ++ [ pkgs.installShellFiles ];
+                buildCommand =
+                  oldAttrs.buildCommand
+                  + ''
+                    completion=${./completions}/${name}.fish
+                    if [[ -e "$completion" ]] then installShellCompletion "$completion"; fi
+                  '';
+              });
           scripts = rec {
             duh = wrap {
               name = "duh";
-              runtimeInputs = [ pkgs.bash pkgs.coreutils ];
+              runtimeInputs = [
+                pkgs.bash
+                pkgs.coreutils
+              ];
             };
             added-lines =
               let
@@ -34,30 +43,47 @@
               };
             silent = wrap {
               name = "silent";
-              runtimeInputs = [ pkgs.bash pkgs.util-linux ];
+              runtimeInputs = [
+                pkgs.bash
+                pkgs.util-linux
+              ];
             };
             xopen = wrap {
               name = "xopen";
-              runtimeInputs = [ pkgs.bash pkgs.xdg-utils silent ];
+              runtimeInputs = [
+                pkgs.bash
+                pkgs.xdg-utils
+                silent
+              ];
             };
             cachedir = wrap {
               name = "cachedir";
               runtimeInputs = [ pkgs.python3 ];
             };
           };
-          headless = with scripts; [ duh added-lines cachedir ];
+          headless = with scripts; [
+            duh
+            added-lines
+            cachedir
+          ];
         in
-        with scripts; scripts // {
+        with scripts;
+        scripts
+        // {
           headless = pkgs.buildEnv {
             name = "headless";
             paths = headless;
           };
           default = pkgs.buildEnv {
             name = "myscripts";
-            paths = headless ++ [ xopen silent ];
+            paths = headless ++ [
+              xopen
+              silent
+            ];
           };
-        });
+        }
+      );
 
-      formatter = forAllPkgs (pkgs: pkgs.nixpkgs-fmt);
+      formatter = forAllPkgs (pkgs: pkgs.nixfmt-rfc-style);
     };
 }
